99b992b4ba27d046cddd72ed8dbcc91c67972d6b
awhipple <a.rgwhip@gmail.com>
2015-01-18 22:43:41 -0800

    Working face detection

--- HybridImage/src/hybridimage/HybridImage.java
+++ HybridImage/src/hybridimage/HybridImage.java
@@ -1,22 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package hybridimage;
 
-/**
- *
- * @author Aaron
- */
-public class HybridImage {
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
+import org.opencv.objdetect.CascadeClassifier;
 
-    /**
-     * @param args the command line arguments
-     */
+class HybridImageDemo {
+  public void run() {
+    System.out.println("\nRunning DetectFaceDemo");
+
+    // Create a face detector from the cascade file in the resources
+    // directory.
+    String faceDetectFileName = getClass().getResource("/resources/lbpcascade_frontalface.xml").getPath();
+    if(faceDetectFileName.startsWith("/", 0)) {
+        faceDetectFileName = faceDetectFileName.replaceFirst("/", "");
+    }
+    CascadeClassifier faceDetector = new CascadeClassifier(faceDetectFileName);
+    
+    String inputImageFileName = getClass().getResource("/resources/images/cat.jpg").getPath();
+    if(inputImageFileName.startsWith("/", 0)) {
+        inputImageFileName = inputImageFileName.replaceFirst("/", "");
+    }
+    Mat image = Highgui.imread(inputImageFileName);
+
+    // Detect faces in the image.
+    // MatOfRect is a special container class for Rect.
+    MatOfRect faceDetections = new MatOfRect();
+    faceDetector.detectMultiScale(image, faceDetections);
+
+    System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
+
+    // Draw a bounding box around each face.
+    for (Rect rect : faceDetections.toArray()) {
+        Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+    }
+
+    // Save the visualized detection.
+    String filename = "faceDetection.png";
+    System.out.println(String.format("Writing %s", filename));
+    Highgui.imwrite(filename, image);
+  }
+}
+
+public class HybridImage {
     public static void main(String[] args) {
-        // TODO code application logic here
+    System.out.println("Hello, OpenCV");
+
+    // Load the native library.
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    new HybridImageDemo().run();
     }
     
 }
