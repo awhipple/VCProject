60aecb6b543535dbc32a1c287518ace7ec25b924
awhipple <a.rgwhip@gmail.com>
2015-01-19 17:58:54 -0800

    First working version of hybrid images

--- HybridImage/src/hybridimage/HybridImage.java
+++ HybridImage/src/hybridimage/HybridImage.java
@@ -2,57 +2,52 @@ package hybridimage;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
-import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Size;
 
 class HybridImageDemo {
   public void run() {
-    System.out.println("\nRunning DetectFaceDemo");
-
-    // Create a face detector from the cascade file in the resources
-    // directory.
-    String faceDetectFileName = getClass().getResource("/resources/lbpcascade_frontalface.xml").getPath();
-    if(faceDetectFileName.startsWith("/", 0)) {
-        faceDetectFileName = faceDetectFileName.replaceFirst("/", "");
-    }
-    CascadeClassifier faceDetector = new CascadeClassifier(faceDetectFileName);
+     
+    final int USE_EXAMPLE = 1;  
     
-    String inputImageFileName = getClass().getResource("/resources/images/cat.jpg").getPath();
+    String[] fileNames = new String[] {
+      "/resources/images/dog2.jpg", "/resources/images/cat2.jpg",
+      "/resources/images/dog3.png", "/resources/images/cat3.png"
+    };
+  
+    String image1FileName = fileNames[USE_EXAMPLE*2];
+    String image2FileName = fileNames[USE_EXAMPLE*2+1];
+    
+    Mat firstImage = loadImage(image1FileName);
+    Mat secondImage = loadImage(image2FileName);
+    
+    Imgproc.GaussianBlur(firstImage, firstImage, new Size(133, 133), 5);
+    
+    Imgproc.Laplacian(secondImage, secondImage, 16, 3, 1, 0);
+    Core.bitwise_not(secondImage, secondImage);
+    
+    Core.bitwise_and(firstImage, secondImage, firstImage);
+    
+    String filename = "result.png";
+    System.out.println(String.format("Writing %s", filename));
+    Highgui.imwrite(filename, firstImage);
+  }
+  
+  private Mat loadImage(String fileName) {
+    String inputImageFileName = getClass().getResource(fileName).getPath();
     if(inputImageFileName.startsWith("/", 0)) {
         inputImageFileName = inputImageFileName.replaceFirst("/", "");
     }
-    Mat image = Highgui.imread(inputImageFileName);
-
-    // Detect faces in the image.
-    // MatOfRect is a special container class for Rect.
-    MatOfRect faceDetections = new MatOfRect();
-    faceDetector.detectMultiScale(image, faceDetections);
-
-    System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
-
-    // Draw a bounding box around each face.
-    for (Rect rect : faceDetections.toArray()) {
-        Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
-    }
-
-    // Save the visualized detection.
-    String filename = "faceDetection.png";
-    System.out.println(String.format("Writing %s", filename));
-    Highgui.imwrite(filename, image);
+    return Highgui.imread(inputImageFileName);
   }
 }
 
 public class HybridImage {
     public static void main(String[] args) {
-    System.out.println("Hello, OpenCV");
+        System.out.println("Hello, OpenCV");
 
-    // Load the native library.
-    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-    new HybridImageDemo().run();
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        new HybridImageDemo().run();
     }
-    
 }
